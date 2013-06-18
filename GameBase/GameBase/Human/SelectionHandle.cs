f44e7b8becb2ef8b0be3e8af85651788ad491f7c
Connor <CMacrascals@yahoo.com>
2013-06-18 20:25:21 +0100

    SelectionHandle

---
+++ GameBase/GameBase/Human/SelectionHandle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameBase.Human
+{
+    class SelectionHandle
+    {
+        InputHandler Input;
+
+        public SelectionHandle()
+        {
+        }
+
+        public void Update(List<Human> HumanList)
+        {
+
+        }
+    }
+}
